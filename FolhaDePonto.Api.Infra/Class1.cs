3f7677c57948ed5659dc286e70fea9acc94c98f6
Gerson Cardoso Filho <gersoncfilho@mac.com>
2018-04-28 12:32:23 -0300

    add infra to project

---
+++ FolhaDePonto.Api.Infra/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FolhaDePonto.Api.Infra
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/FolhaDePonto.Api.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FolhaDePonto.Api.Domain
+{
+    public class Class1
+    {
+    }
+}
